1247e765184ea6c9025a478b836a1b480bbd0bab
GerganaDimitrova <dimitrovag97@abv.bg>
2016-11-08 11:49:28 +0200

    task03

--- task02.cpp
+++ task02.cpp
@@ -8,20 +8,10 @@ using namespace std;
 
 int main()
 {
-	int x, y;
-	cin >> x >> y;
-	int S = 1;
-	if (x < y) {
-		for (int i = x; i <= y; i++) {
-			S = S*i;
-		}
-		cout << S << endl;
-	}
-	else 
-		for (int i = y; i <= x; i++) {
-			S = S*i;
-		}
-	cout << S << endl;
+	int n;
+	cin >> n;
+
+	cout << (n*(n + 1)) / 2 << endl;
 
     return 0;
 }
